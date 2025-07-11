f2cc48fa769572bbb87b1b46d9b6c1250dc0746f
Thaila Mariane Agard <thaila.agard@sensedia.local>
2025-07-11 09:39:06 -0300

    exemplo de cenario de test

---
+++ tests/example.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Login com Sucesso', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('teste@gmail.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+});
+
